020a9a8d4eecbadee628c50f643285266e781cc1
tanmayk5 <73903781+tanmayk5@users.noreply.github.com>
2021-05-05 11:21:30 +0530

    counter added

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ if (mute=='true') {
 }
 
 
+
 var list = document.querySelector("#ordered-list");
 var listContainer = document.querySelector('.todo-list-container');
 
@@ -33,6 +34,8 @@ muteUnmute.addEventListener("click", function () {
   localStorage.setItem("muteSetting", mute);
 });
 var input = document.querySelector("#input-task");
+var counter = document.querySelector('#counter');
+counter.innerText = `(${list.children.length})`
 addTask.addEventListener("click", function () {
   if (input.value.length > 0) {
     if (!mute) {
@@ -55,8 +58,11 @@ addTask.addEventListener("click", function () {
     if (list.innerText != "") {
       listContainer.style.backgroundImage = "none";
     }
+  counter.innerText = `(${list.children.length})`;
+
   }
 });
+
 
 document.addEventListener("keypress", function (e) {
   if (e.key == "Enter" && input.value.length > 0) {
@@ -81,8 +87,12 @@ document.addEventListener("keypress", function (e) {
     if (list.innerText != "") {
       listContainer.style.backgroundImage = "none";
     }
+  counter.innerText = `(${list.children.length})`;
+
+    // console.log(list.children.length)
   }
 });
+
 
 var audio = document.querySelector("#sound");
 audio.preload = "auto";
@@ -98,12 +108,14 @@ list.addEventListener("click", function (e) {
     if (list.innerText == "") {
       listContainer.style.backgroundImage = "url(./imgs/empty.svg)";
     }
+counter.innerText = `(${list.children.length})`;
+
     localStorage.setItem("storedtodolist", list.innerHTML);
   } else if (e.target.className == "done") {
     if (li.style.color == "gray") {
       li.style.color = "white";
       li.querySelector(".task-text").style.textDecoration = "none";
-
+      counter.innerText = `(${list.children.length})`;
       li.querySelector(".done").style.backgroundImage = "url(./imgs/done.svg)";
       li.querySelector(".done").style.backgroundColor = "rgb(31, 223, 31)";
       localStorage.setItem("storedtodolist", list.innerHTML);
@@ -128,4 +140,6 @@ clear.addEventListener('click',function() {
   list.innerHTML='';
   listContainer.style.backgroundImage = "url(./imgs/empty.svg)";
   localStorage.setItem("storedtodolist", list.innerHTML);
+counter.innerText = `(${list.children.length})`;
+
 })
